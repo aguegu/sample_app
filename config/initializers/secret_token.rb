c31460ec84360f8a50fc83c01f015f41f0529c4e
Weihong Guan <weihong.guan@gmail.com>
2013-05-01 00:32:59 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SamlpeApp::Application.config.secret_token = '2cef5599174b3274be78645dd98f872095dc1547f7f03214facf42453750911d4c558a6935c7e221d37a7f3e0a3f25fb3d7baa7a7d1376145de3e21db1fcc975'
